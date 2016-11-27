abc4d4a31544cd365a85317fcc8613c59de1fc7c
db118 <d_b118@txstate.edu>
2016-11-26 18:43:04 -0600

    finished with collecting data

--- machLearn/stock_info.py
+++ machLearn/stock_info.py
@@ -19,7 +19,33 @@ usholidays16 = [str(date(2016,1,1)), str(date(2016,1,18)), str(date(2016,2,15)),
                 str(date(2017,7,3)), str(date(2017,7,4)), str(date(2017,9,4)),
                 str(date(2017,11,23)), str(date(2017,11,24)), str(date(2017,12,25))]
 
+def create_date(date):
+	string_date = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
+	return string_date
 
+def check_weekend(date):
+	num_day = date.weekday()
+	if num_day > 4:
+		return True
+	else:
+		return False
+
+def change_weekend(date):
+	if check_weekend(date):
+		num_day = date.weekday()
+		date = date - timedelta(days = num_day-4)
+	return date
+
+def check_holiday(date):
+	if str(date.date()) in usholidays16:
+		return True
+	else:
+		return False 
+
+def change_holiday(date):
+	while check_holiday(date):
+		date = date - timedelta(days = 1)
+	return date
 
 def get_older_dates(day):
 	date = datetime.now()-timedelta(days=day)
@@ -27,19 +53,13 @@ def get_older_dates(day):
 	for i in range(3):
 		if i != 0:
 			date = date - timedelta(days = 1)
-		num_day = date.weekday()
-		if num_day ==  5:
-			date = (date - timedelta(days = 1))
-			new_date = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
-			date_list.append(new_date)
-		elif num_day == 6:
-			date = (date - timedelta(days = 2))
-			new_date = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
-			date_list.append(new_date)
-		else:
-			new_date = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
-			date_list.append(new_date)
+		if check_weekend(date):
+			date = change_weekend(date)
+		if check_holiday(date):
+			date = change_holiday(date)
 
+		new_date = create_date(date)
+		date_list.append(new_date)
 	return date_list
 
 
@@ -62,13 +82,11 @@ def stock_data(start, end,companies):
 	             'Volume': [],
 	             'Date': []
 	}
-	count = 0
 	for key,value in companies.items()[:2]:
 		company = Share(key)
 		company.refresh()
 		data = company.get_historical(start,end)
 		for item in data:
-			count += 1
 			raw_data['Company'].append(value)
 			raw_data['High'].append(float(item['High']))
 			raw_data['Low'].append(float(item['Low']))
@@ -76,7 +94,6 @@ def stock_data(start, end,companies):
 			raw_data['Close'].append(float(item['Close']))
 			raw_data['Volume'].append(int(item['Volume']))
 			raw_data['Date'].append(item['Date'])
-	print count 
 	return raw_data
 
 def make_dataframe(raw_data):
@@ -99,34 +116,30 @@ def make_dataframe(raw_data):
 
 def get_close_price(start,end,companies):
 	close_price = {'Close': [], 'Date': []}
-	count = 0
 	for symbol in companies.keys()[:2]:
 		company = Share(symbol)
 		company.refresh()
 		data = company.get_historical(start,end)
 		for item in data: 
-			count +=1
 			close_price['Close'].append(float(item['Close']))
 			close_price['Date'].append(item['Date'])
-	print count
 	return close_price
 
-print usholidays16
-# nyse_symbols = finsym.get_nyse_symbols()
-# tech_companies = {item['symbol'].strip():item['company'] for item in nyse_symbols if 'Technology' in item.values()}
-# tech_symbols = get_symbols(tech_companies)
+nyse_symbols = finsym.get_nyse_symbols()
+tech_companies = {item['symbol'].strip():item['company'] for item in nyse_symbols if 'Technology' in item.values()}
+tech_symbols = get_symbols(tech_companies)
 
-# sample_day = 5
-# date_now = get_older_dates(0)
-# old_date = get_older_dates(sample_day)
+sample_day = 5
+date_now = get_older_dates(0)
+old_date = get_older_dates(sample_day)
 
-# raw_data = stock_data(old_date[0],date_now[0],tech_symbols)
-# shift_close1 = get_close_price(old_date[1],date_now[1],tech_symbols)
-# shift_close2 = get_close_price(old_date[2],date_now[2],tech_symbols)
-# stock_numbers = make_dataframe(raw_data)
-# greater = lambda x : 1 if x == True else 0     # 1 if today's price is greater than yesterday's price else 0 
-# stock_numbers['Compare1'] = stock_numbers['Close'] > shift_close1['Close']
-# stock_numbers['Compare2'] = stock_numbers['Close'] > shift_close2['Close']
-# stock_numbers['Compare1'] = stock_numbers['Compare1'].apply(greater)
-# stock_numbers['Compare2'] = stock_numbers['Compare2'].apply(greater)
-# stock_numbers.to_csv('stock_info.csv')#, mode='a')
+raw_data = stock_data(old_date[0],date_now[0],tech_symbols)
+shift_close1 = get_close_price(old_date[1],date_now[1],tech_symbols)
+shift_close2 = get_close_price(old_date[2],date_now[2],tech_symbols)
+stock_numbers = make_dataframe(raw_data)
+greater = lambda x : 1 if x == True else 0     # 1 if today's price is greater than yesterday's price else 0 
+stock_numbers['Compare1'] = stock_numbers['Close'] > shift_close1['Close']
+stock_numbers['Compare2'] = stock_numbers['Close'] > shift_close2['Close']
+stock_numbers['Compare1'] = stock_numbers['Compare1'].apply(greater)
+stock_numbers['Compare2'] = stock_numbers['Compare2'].apply(greater)
+stock_numbers.to_csv('stock_info.csv')#, mode='a')
