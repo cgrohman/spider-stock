5b30a01c92732740efdc92125c39345799a8a254
Cori Grohman <corigrohman@gmail.com>
2016-11-28 08:14:10 -0600

    began refactoring the text scrape logic

--- spider/newsSpider.py
+++ spider/newsSpider.py
@@ -23,9 +23,6 @@ from scrapy import Request
 from html.parser import HTMLParser
 import re
 
-XPATH_SELECTOR = '//*[@id="storytext"]/*[self::h2 or self::p]'
-AUTHOR_CSS = '.byline::text'
-AUTHOR_CSS_A = '.byline a::text'
 DATE_CSS = '.cnnDateStamp::text'
 DENY = [r'/quote.*',r'/video.*',r'/services/privacy/',r'/services/terms.html']
 ALLOW = [r'money.cnn.com']
@@ -33,26 +30,43 @@ TEXT_FIND = r'trump'
 
 class NewsSpider(CrawlSpider):
 	name = 'crawltest'
-	allowed_domains = [r'money.cnn.com']
-	deny_domains = [r'com/quote']
 	start_urls = ['http://money.cnn.com/']
+
+	XPATH_SELECTOR = '//*[@id="storytext"]/*[self::h2 or self::p]'
+	AUTHOR_CSS = '.byline::text'
+	AUTHOR_CSS_A = '.byline a::text'
+	regex = re.compile(TEXT_FIND, re.IGNORECASE)
+
 	rules = (Rule(LinkExtractor(allow=ALLOW, deny = DENY), callback='parse_page', follow=True),)
 
 	def parse_page(self,response):
-		regex = re.compile(TEXT_FIND, re.IGNORECASE)
-		text = response.xpath(XPATH_SELECTOR).extract()
-		text = ''.join(text)
-		if regex.search(text) is None: return
-		text = strip_tags(text)
-		author = response.css(AUTHOR_CSS_A).extract_first()
-		if author is None:
-			author = response.css(AUTHOR_CSS).extract_first()
-			author = re.sub('by ','',author)
+		article_text = self.get_text(response)
+		if article_text is None: 
+			#log cannot find error
+			return
+		author_text = self.get_author_text(response)
+		if author_text is None:
+			author_text = 'NA'
 		date_pub = response.css(DATE_CSS).extract_first()
-		yield{'text':text,
-			'author':author.strip(),
+		yield{'text':article_text,
+			'author':author_text.strip(),
 			'link':response.url,
 			'date':date_pub.strip()}
+
+	def get_text(self,response):
+		text_list = response.xpath(self.XPATH_SELECTOR).extract()
+		if not text_list: return None
+		text = ''.join(text_list)
+		article_text = strip_tags(text)
+		return article_text
+
+	def get_author_text(self,response):
+		author_text = response.css(self.AUTHOR_CSS_A).extract_first()
+		if author_text is None:
+			author = response.css(self.AUTHOR_CSS).extract_first()
+			if author is None: return None
+			author_text = re.sub('by ','',author)
+		return author_text
 
 #Used for stripping text of html tags, returns only text
 class MLStripper(HTMLParser):
