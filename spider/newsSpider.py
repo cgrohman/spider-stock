354b5116d83c2c22845223522f5c7dd2385c8be8
Cori Grohman <corigrohman@gmail.com>
2016-11-27 20:03:02 -0600

    updated some mino rindention issues

--- spider/newsSpider.py
+++ spider/newsSpider.py
@@ -15,42 +15,41 @@ TEXT_FIND = r'trump'
 
 
 class NewsSpider(CrawlSpider):
-  name = 'crawltest'
-  allowed_domains = [r'money.cnn.com']
-  deny_domains = [r'com/quote']
-  start_urls = ['http://money.cnn.com/']
+	name = 'crawltest'
+	allowed_domains = [r'money.cnn.com']
+	deny_domains = [r'com/quote']
+	start_urls = ['http://money.cnn.com/']
+	rules = (Rule(LinkExtractor(allow=ALLOW, deny = DENY), callback='parse_page', follow=True),)
 
-  rules = (Rule(LinkExtractor(allow=ALLOW, deny = DENY), callback='parse_page', follow=True),)
-
-  def parse_page(self,response):
-    regex = re.compile(TEXT_FIND, re.IGNORECASE)
-    text = response.xpath(XPATH_SELECTOR).extract()
-    text = ''.join(text)
-    if regex.search(text) is None: return
-    text = strip_tags(text)
-    author = response.css(AUTHOR_CSS_A).extract_first()
-    if author is None:
-      author = response.css(AUTHOR_CSS).extract_first()
-      author = re.sub('by ','',author)
-    date_pub = response.css(DATE_CSS).extract_first()
-    yield{'text':text,
-          'author':author.strip(),
-          'link':response.url,
-          'date':date_pub.strip()}
+	def parse_page(self,response):
+		regex = re.compile(TEXT_FIND, re.IGNORECASE)
+		text = response.xpath(XPATH_SELECTOR).extract()
+		text = ''.join(text)
+		if regex.search(text) is None: return
+		text = strip_tags(text)
+		author = response.css(AUTHOR_CSS_A).extract_first()
+		if author is None:
+			author = response.css(AUTHOR_CSS).extract_first()
+			author = re.sub('by ','',author)
+		date_pub = response.css(DATE_CSS).extract_first()
+		yield{'text':text,
+			'author':author.strip(),
+			'link':response.url,
+			'date':date_pub.strip()}
 
 #Used for stripping text of html tags, returns only text
 class MLStripper(HTMLParser):
-		def __init__(self):
-				super().__init__()
-				self.reset()
-				self.fed = []
-		def handle_data(self, d):
-				self.fed.append(d)
-		def get_data(self):
-				return ''.join(self.fed)
+	def __init__(self):
+		super().__init__()
+		self.reset()
+		self.fed = []
+	def handle_data(self, d):
+		self.fed.append(d)
+	def get_data(self):
+		return ''.join(self.fed)
 
 # Strips string of html tags
 def strip_tags(html):
-		s = MLStripper()
-		s.feed(html)
-		return s.get_data()
+	s = MLStripper()
+	s.feed(html)
+	return s.get_data()
