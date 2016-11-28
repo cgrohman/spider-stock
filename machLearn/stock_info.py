6919f5a09f9caf8aac15e4956c395927243a9345
Darryl Balderas <d_b118@txstate.edu>
2016-11-28 12:04:06 -0600

    Update stock_info.py

--- machLearn/stock_info.py
+++ machLearn/stock_info.py
@@ -129,7 +129,7 @@ nyse_symbols = finsym.get_nyse_symbols()
 tech_companies = {item['symbol'].strip():item['company'] for item in nyse_symbols if 'Technology' in item.values()}
 tech_symbols = get_symbols(tech_companies)
 
-sample_day = 7
+sample_day = 7  #will be change to 365 days 
 date_now = get_older_dates(0)
 old_date = get_older_dates(sample_day)
 raw_data = stock_data(old_date[0],date_now[0],tech_symbols)
@@ -137,12 +137,9 @@ shift_close1 = get_close_price(old_date[1],date_now[1],tech_symbols)
 shift_close2 = get_close_price(old_date[2],date_now[2],tech_symbols)
 stock_numbers = make_dataframe(raw_data)
 
-print raw_data
-print shift_close1
-print shift_close2
 greater = lambda x : 1 if x == True else 0     # 1 if today's price is greater than yesterday's price else 0 
 stock_numbers['Compare1'] = stock_numbers['Close'] > shift_close1['Close']
 stock_numbers['Compare2'] = stock_numbers['Close'] > shift_close2['Close']
 stock_numbers['Compare1'] = stock_numbers['Compare1'].apply(greater)
 stock_numbers['Compare2'] = stock_numbers['Compare2'].apply(greater)
-stock_numbers.to_csv('stock_info.csv')#, mode='a')
+stock_numbers.to_csv('stock_info.csv')            #, mode='a')
