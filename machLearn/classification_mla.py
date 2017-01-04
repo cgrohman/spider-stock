7dd2b40f2a6425491da73d85d921a86e116661f6
db118 <d_b118@txstate.edu>
2017-01-03 19:43:49 -0600

    feat: comparing differnt machine learning algorithms
    removed redundant code and organize the flow of the machine
    learning program. Doing machine learning comparison, but
    ran into problem of needed more data in order to do
    kfold cross validation

--- machLearn/classification_mla.py
+++ machLearn/classification_mla.py
@@ -1,38 +1,44 @@
 import math 
 import pandas  as pd
 from pandas.tools.plotting import scatter_matrix
-from pandas import set_option
 import numpy as np 
-import scipy 
 import matplotlib.pyplot as plt
-from numpy import set_printoptions
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
+plt.style.use('ggplot')
+pd.set_option('display.width', 5000) 
+pd.set_option('display.max_columns', 60) 
 
+file_name = 'stock_info.csv'
 names = ['Volume','Company','High',
          'Low,Date','Close','Open','Outcome',
          'Ho','Lo','Gain','Compare1','Compare2'
          ]
 
-data = pd.read_csv('stock_info.csv')
+data = pd.read_csv(file_name)
 
-# set_option('display.width',100)
+#This column does not belong in the dataframe
+data = data.drop('Unnamed: 0',1)  
 
-set_option('precision',3)
 
 #sort the dataframe descending
-
 data_size = data.shape
-
 peek = data.head(20)
 
 var_type = data.dtypes
 
 #Descritptive Statistics
-
 description = data.describe()
 
 outcome = data.groupby('Outcome').size()
@@ -46,48 +52,68 @@ correlations = data.corr(method = 'pearson')
 skew = data.skew()
 
 
-#Visualization
+# #Visualization
+# data.hist()
+# data.plot(kind='density',subplots=True, layout =(4,3), sharex = False)
+# data.plot(kind = 'box', subplots = True, layout=(4,3), sharex = False, sharey = False)
 
-data.hist()
+# #correlation matrix
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# cax = ax.matshow(correlations , vmin=-1, vmax=1)
+
+# fig.colorbar(cax)
+# ticks = np.arange(0,12,1)
+# ax.set_xticks(ticks)
+# ax.set_yticks(ticks)
+# ax.set_xticklabels(names)
+# ax.set_yticklabels(names)
+
+# scatter_matrix(data)
+# plt.show()
+
+array = data.values
+
+X = array[:,:10]
+Y = array[:,11:]
+
+test_size = 0.33
+
+seed = 7
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,
+	test_size = test_size, random_state=seed)
+
+models = []
+models.append(( 'LR' , LogisticRegression()))
+models.append(( 'LDA' , LinearDiscriminantAnalysis()))
+models.append(( 'KNN' , KNeighborsClassifier()))
+models.append(( 'CART' , DecisionTreeClassifier()))
+models.append(( 'NB' , GaussianNB()))
+models.append(( 'SVM' , SVC()))
+
+# # evaluate each model in turn
+# results = []
+# names = []
+# scoring = 'accuracy'
+# for name, model in models:
+#   kfold = KFold(n_splits=10, random_state=seed)
+#   cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring = scoring )
+#   results.append(cv_results)
+#   names.append(name)
+#   msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+#   print(msg)
 
 
-data.plot(kind='density',subplots=True, layout =(4,3), sharex = False)
+# # Compare Algorithms
+# fig = plt.figure()
+# fig.suptitle( 'Algorithm Comparison')
+# ax = fig.add_subplot(111)
+# plt.boxplot(results)
+# ax.set_xticklabels(names)
+# plt.show()
 
 
-data.plot(kind = 'box', subplots = True, layout=(4,3), sharex = False, sharey = False)
-
-
-#correlation matrix
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(correlations , vmin=-1, vmax=1)
-
-fig.colorbar(cax)
-ticks = np.arange(0,12,1)
-ax.set_xticks(ticks)
-ax.set_yticks(ticks)
-ax.set_xticklabels(names)
-ax.set_yticklabels(names)
-
-scatter_matrix(data)
-plt.show()
-
-#data transformation
-numpy.set_printoptions(precision=3)
-values = data.values
-attributes = values[:,0:11]
-predict = values[:,11]
-minmax_scaler = MinMaxScaler(feature_range = (0,1))
-standard_scaler = StandardScaler.fit(attributes)
-minmax_scaled_attributes = scaler.fit_transform(attributes)
-standar_scaled_attributes = standard_scaler.transform(attributes)
-
-
-#feature selection
-test = SelectKBest(score_func = chi2, k = 3)
-fit = test.fit(attributes,predict)
-weight_features_value = fit.scores_
-features = fit.transform(attributes)
 
 
 
